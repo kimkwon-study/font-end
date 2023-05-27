d80afc069cfd246a04ad361ae1669e31fee4842e
Youo Kim <albert050509@naver.com>
2023-05-28 02:27:20 +0900

    test login data

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,6 +1,42 @@
+import '../Styles/Main.css'
+
 function Login() {
+
+    async function request() {
+        const response = await fetch('http://localhost:3000/api/login',{
+            method : 'GET',
+        })
+        const data = await response.json();
+        console.log(data)
+    }
+
+    
+
     return (
-        <div>this is Login pages</div>
+        <>
+            <div className = "login-container">
+                <form action = "/api/login" method = "POST" id = "login-form">
+                    <div className = "title-box">Login</div>
+                    <div className = "input-box">
+                        <label for = "userId">userId</label>
+                        <input type = "text" id = "userId"></input>
+                    </div>
+                    <div className = "input-box">
+                        <label for = "password">password</label>
+                        <input type = "password" id = "password"></input>
+                    </div>
+                    <div className = "check-box">
+                        <input type="checkbox" id="check1"/>
+                        <label for="check1"></label>
+                        <a href = "#">Forget Password?</a>
+                    </div>
+                    <div className = "btn-box">
+                        <input type = "submit" value = "Login" id = "login-btn"></input>
+                    </div>
+                    <div className = "next-page-box"><a href = "/register">go to register page</a></div>
+                </form>
+            </div>
+        </>
     )
 }
 
