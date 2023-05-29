4c79ea054617db9ff0a032dbdbaf5db7d27ac483
Nayoung <skdud3669@gmail.com>
2023-05-29 17:58:34 +0900

    register without token

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,73 +1,115 @@
 import { useForm } from 'react-hook-form'
+
+import axios from 'axios'
 import './styles.css'
-import { Button, Col, Container, FloatingLabel, Form, Row } from 'react-bootstrap'
+import { Col, Container, FloatingLabel, Form, Row } from 'react-bootstrap'
 
 const Signup = () => {
     const {
         register,
-        handlerSubmit,
-        formState : { errors },
+        handleSubmit,
+        formState : { errors, isSubmitting },
     } = useForm();
 
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data))
+        axios.post('http://localhost:8080/api/register', JSON.stringify(data))
+            .then(function (response) {
+                console.log(response);
+                if (response.result_code === "REGISTER_SAME_ID") {
+                    alert(response.result)
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
     return (
-            <Container className='signupCard' >
-                <form className='form' >
+            <Container className='signupCard' gap={3} >
+                <form className='form' onSubmit={handleSubmit(onSubmit)} >
                     <Row>
                         <p className='signupText'>Sign up</p>
                     </Row>
-                    <Row >
-                        <Form.Group className="mb-3" controlId="userId">
-                            <FloatingLabel label="아이디" >
-                                <Form.Control {...register('userId' ,{ required: true })} type="text" placeholder="아이디를 입력해주세요."  />
+                    <Row>
+                        <Col >
+                            <Form.Group className="mb-3" controlId="userId">
+                                <FloatingLabel label="아이디" >
+                                    <Form.Control {...register('userId' ,{
+                                        required: "아이디는 필수 입력입니다.",
+                                        })} type="text" />
+                                    <Form.Text className="text-muted">
+                                        {errors.userId && errors.userId.message}
+                                    </Form.Text>
+                                </FloatingLabel>
+                            </Form.Group>
+                        </Col>
+                        {/* <Col sm={3}>
+                            <button className='idCheck'>중복확인</button>
+                        </Col> */}
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Group className="mb-3" controlId="password">
+                                <FloatingLabel label="비밀번호(10자리 이상)" className="mb-3" >
+                                    <Form.Control 
+                                        {...register('password' ,{ 
+                                            required: "비밀번호는 필수 입력입니다.",
+                                            minLength: {
+                                                value: 8,
+                                                message: "8자리 이상 비밀번호를 사용하세요.",
+                                            },
+                                        })} type="password" />
+                                    <Form.Text className="text-muted">
+                                        {errors.password && errors.password.message}
+                                    </Form.Text>
+                                </FloatingLabel>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="mb-3" >
+                                <FloatingLabel label="비밀번호 확인" className="mb-3" >
+                                    <Form.Control type="password"/>
+                                    <Form.Text className="text-muted">
+                                        {errors.password && errors.password.message}
+                                    </Form.Text>
+                                </FloatingLabel>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Group className="mb-3" controlId="nickname">
+                                <FloatingLabel label="닉네임" className="mb-3" >
+                                    <Form.Control {...register('nickname' ,{ required: "닉네임은 필수 입력입니다." })} type="text" />
+                                    <Form.Text className="text-muted">
+                                        {errors.nickname && errors.nickname.message}
+                                    </Form.Text>
+                                </FloatingLabel>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Group className="mb-3" controlId="email">
+                                <FloatingLabel label="이메일 주소" className="mb-3">
+                                    <Form.Control {...register('email' ,{ 
+                                        required: "이메일은 필수 입력입니다.",
+                                        pattern: {
+                                            value: /\S+@\S+\.\S+/,
+                                            message: "이메일 형식에 맞지 않습니다.",
+                                        }
+                                    })} type="email" />
+                                </FloatingLabel>
                                 <Form.Text className="text-muted">
-                                    사용가능한 아이디입니다.
+                                    {errors.email && errors.email.message}
                                 </Form.Text>
-                            </FloatingLabel>
-                        </Form.Group>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <Form.Group className="mb-3" controlId="userPassword">
-                                <FloatingLabel label="비밀번호(10자리 이상)" className="mb-3" controlId="userPassword">
-                                    <Form.Control type="password" placeholder="비밀번호를 입력해주세요." />
-                                    <Form.Text className="text-muted">
-                                        올바르지 않은 비밀번호 형식입니다.
-                                    </Form.Text>
-                                </FloatingLabel>
-                        </Form.Group>
-
-                        </Col>
-                        <Col>
-                            <Form.Group className="mb-3" controlId="userPasswordCheck">
-                                <FloatingLabel label="비밀번호 확인" className="mb-3" controlId="userPassword">
-                                    <Form.Control type="password" placeholder="비밀번호를 입력해주세요." />
-                                    <Form.Text className="text-muted">
-                                        비밀번호가 불일치합니다.
-                                    </Form.Text>
-                                </FloatingLabel>
-                        </Form.Group>
+                            </Form.Group>
                         </Col>
                     </Row>
-                    <Row>
-                        <Form.Group className="mb-3" controlId="userNickname">
-                            <FloatingLabel label="닉네임" className="mb-3" controlId="userNickname">
-                                <Form.Control type="text" placeholder="닉네임을 입력해주세요." />
-                                {/* <Form.Text className="text-muted">
-                                    사용가능한 닉네임입니다.
-                                </Form.Text> */}
-                            </FloatingLabel>
-                        </Form.Group>
-                    </Row>
-                    <Row>
-                        <Form.Group className="mb-3" controlId="userEmail">
-                            <FloatingLabel label="이메일 주소" className="mb-3" controlId="userEmail">
-                                <Form.Control type="email" placeholder="이메일을 입력해주세요." />
-                            </FloatingLabel>
-                        </Form.Group>
-                    </Row>
-                    <Button type="submit" className='signupButton'>
+                    <button type="submit" className='signupButton' disabled={isSubmitting}>
                         회원가입
-                    </Button>
+                    </button>
                 </form>
             </Container>
     )
